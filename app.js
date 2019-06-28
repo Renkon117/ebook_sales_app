c0747e9bffd9c45cad4fee102646ac2c25628903
okada001 <okada001@rangers.uwp.edu>
2019-06-28 14:52:10 +0900

    added charge route and connected to api

--- app.js
+++ app.js
@@ -1,11 +1,50 @@
 require("dotenv").config();
 
+const keys = require("./config/keys");
+
 const express = require("express");
-const stripe = require("stripe")(process.env.SECRET_KEY);
+const stripe = require("stripe")(keys.stripeSecretKey);
 const bodyParser = require("body-parser");
 const exphbs = require("express-handlebars");
 
 const app = express();
+
+//handlebars middleware
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//bodyParser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//set the static folder
+app.use(express.static(`${__dirname}/public`));
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    stripePublishableKey: keys.stripePublishableKey
+  });
+});
+
+//charge route
+app.post("/charge", (req, res) => {
+  const amount = 2500;
+
+  stripe.customers
+    .create({
+      email: req.body.stripeEmail,
+      source: req.body.stripeToken
+    })
+    .then(customer =>
+      stripe.charges.create({
+        amount,
+        description: "web development ebook",
+        currency: "usd",
+        customer: customer.id
+      })
+    )
+    .then(charge => res.render("success"));
+});
 
 const port = process.env.PORT || 5000;
 
